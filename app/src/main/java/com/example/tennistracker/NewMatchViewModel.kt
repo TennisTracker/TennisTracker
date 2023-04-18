7d60a152a11e9e40d71278f382f8b9230f9f719f
Cody Korman <ckorman@gustavus.edu>
2023-04-17 22:22:24 -0500

    Created ViewModel, transfered variables to ViewModel. NewMatch is hard to read now sorry

---
+++ app/src/main/java/com/example/tennistracker/NewMatchViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.tennistracker
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+
+class NewMatchViewModel(private  val savedStateHandle: SavedStateHandle): ViewModel(){
+    var totalpoints = 0
+    var P1totalpoints = 0
+    var P2totalpoints = 0
+    var P1index = 0
+    var P2index = 0
+    var P1gameIndex = 0
+    var P2gameIndex = 0
+    var P1tiebreakScore = 0
+    var P2tiebreakScore = 0
+    var setIndexP1 = 0
+    var setIndexP2 = 0
+}
