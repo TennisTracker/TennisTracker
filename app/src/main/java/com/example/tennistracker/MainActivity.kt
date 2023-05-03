9b19ea20e52b58c99da1f43d2444843b55583284
DanielFouchier <dfouchie@gustavus.edu>
2023-05-02 19:50:48 -0500

    Test Buzz

--- app/src/main/java/com/example/tennistracker/MainActivity.kt
+++ app/src/main/java/com/example/tennistracker/MainActivity.kt
@@ -1,22 +1,29 @@
 package com.example.tennistracker
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-
+import com.example.tennistracker.R
 class MainActivity : AppCompatActivity() {
 
 
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //This gets button id, when we click on new match button we go to activity, NewMatch
         val newMatchButton = findViewById<Button>(R.id.btnNewMatch)
+        val trackServeButton = findViewById<Button>(R.id.btnTrackServes)
         newMatchButton.setOnClickListener{
             val Intent = Intent(this, NewMatch::class.java)
             startActivity(Intent)
         }
+        trackServeButton.setOnClickListener{
+            val intent = Intent(this, ServeTracker::class.java)
+            startActivity(intent)
+        }
+        }
     }
-}
---
+++ app/src/main/java/com/example/tennistracker/ServeTracker.kt
@@ -0,0 +1,19 @@
+package com.example.tennistracker
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+
+class ServeTracker: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_servespot_tracker)
+    }
+
+}
+
