baca4ec4854459861fd2142b14c9141947708092
seanNZH <sean@bochman.net>
2023-05-05 14:05:16 -0500

    fixed small issue where serves no longer appears in the summary page. Then added color to be different depending on if return was made or missed

--- app/src/main/java/com/example/tennistracker/ServeTrackerSum.kt
+++ app/src/main/java/com/example/tennistracker/ServeTrackerSum.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.RelativeLayout
@@ -21,19 +22,25 @@ class ServeTrackerSum: AppCompatActivity() {
 
 
 
-        for (pair in coordinateList) {
-            placeDot(pair.first, pair.second, mRelativeLayout)
+        for (triple in coordinateList) {
+            if (triple.third == false){
+                placeDot(triple.first, triple.second, mRelativeLayout, "RED")
+            }
+            else{
+                placeDot(triple.first, triple.second, mRelativeLayout, "GREEN")
+            }
+
         }
 
 
     }
 
 
-    public fun placeDot(x: Float, y: Float, parentView: ViewGroup) {
+    public fun placeDot(x: Float, y: Float, parentView: ViewGroup, color: String) {
         // Create a new ImageView for the dot
         val dot = ImageView(this)
         dot.setImageResource(R.drawable.dot)
-        dot.setColorFilter(Color.RED)
+        dot.setColorFilter(Color.parseColor(color))
         val dotSize = 30
         val params = RelativeLayout.LayoutParams(dotSize, dotSize)
         params.leftMargin = (x - dotSize / 2).toInt()
